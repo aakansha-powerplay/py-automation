f3b8483fd461055e9106b4daaafc445df2d71ffd
abhinavpowerplay <144247760+abhinavpowerplay@users.noreply.github.com>
2025-09-30 12:24:37 +0530

    Update final_activity_report_share.py

--- final_activity_report_share.py
+++ final_activity_report_share.py
@@ -30,13 +30,16 @@ print(f"✅ Loaded {len(data)} rows from Google Sheet.")
 chrome_options = Options()
 # ⬇ Disable headless to see QR and scan manually
 # chrome_options.add_argument("--headless=new")
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")  # use new headless mode
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-software-rasterizer")
 chrome_options.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(options=chrome_options)
 print("✅ Chrome driver setup done.")
-
 # =========================
 #  WHATSAPP LOGIN - MANUAL QR
 # =========================
